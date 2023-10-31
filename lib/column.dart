e23f1ec972e0bbbdabfca5388bb965786609239d
Ahsan <ahsanaz461@gmail.com>
2023-10-31 14:09:53 +0700

    add column.dart

---
+++ lib/column.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ColumnExample extends StatelessWidget {
+  const ColumnExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh Column Widget")),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.purpleAccent,
+              child: const FlutterLogo(
+                size: 90.0,
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              child: const FlutterLogo(
+                size: 90.0,
+              ),
+            ),
+            Container(
+              color: Colors.orangeAccent,
+              child: const FlutterLogo(
+                size: 90.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
